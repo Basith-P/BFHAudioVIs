cb06717d52327e26712be6ee6053a1211d94717e
Basith P <basithpofficial@gmail.com>
2021-05-21 21:03:21 +0530

    Adding audio upload functionality

--- website/models.py
+++ website/models.py
@@ -1,18 +1,20 @@
+from enum import unique
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
+class audioDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    audio = db.Column(db.Text, unique=False, nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    mimetype = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    audios = db.relationship('audioDB')
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from os import name
+from flask import Flask, Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from werkzeug.utils import secure_filename
+from .models import audioDB, User
 from . import db
 import json
 
@@ -9,29 +11,50 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
-def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
+# def home():
+    # if request.method == 'POST':
+    #     note = request.form.get('note')
 
-        if len(note) < 1:
-            flash('Note is too short!', category='error')
+    #     if len(note) < 1:
+    #         flash('Note is too short!', category='error')
+    #     else:
+    #         new_note = Note(data=note, user_id=current_user.id)
+    #         db.session.add(new_note)
+    #         db.session.commit()
+    #         flash('Note added!', category='success')
+
+    # return render_template("home.html", user=current_user)
+
+
+# @views.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        audio = request.files['aud-in']
+
+        if not audio:
+          flash("Couldn't upload the audio!", category='error')
         else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
+            filename = secure_filename(audio.filename)
+            mimetype = audio.mimetype
+            audiofile = audioDB(audio=audio.read(), mimetype=mimetype, name=filename, user_id=current_user.id)
+            db.session.add(audiofile)
             db.session.commit()
-            flash('Note added!', category='success')
+            flash('Audio uploaded!', category='success')
 
     return render_template("home.html", user=current_user)
 
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
+
+
+
+@views.route('/delete-audio', methods=['POST'])
+def delete_audio():
+    audio = json.loads(request.data)
+    audioId = audio['audioId']
+    audio = audio.query.get(audioId)
+    if audio:
+        if audio.user_id == current_user.id:
+            db.session.delete(audio)
             db.session.commit()
 
     return jsonify({})
